98f61c46dc914249c05be777e4a9796699eb5545
nat corpuz <nate.corpuz1@gmail.com>
2022-05-09 17:44:10 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles/00-main.css";
+import "./styles/00-reset.css";
+import "./styles/01-header.css";
+import "./styles/02-about.css";
+import "./styles/03-tech-stack.css";
+import "./styles/04-process.css";
+import "./styles/05-projects.css";
+import "./styles/06-contact.css";
+import "./styles/07-footer.css";
